c858cf7f3edb552963be0842578b4805e8623944
Prarthana-sudeesh <prarthana.sudeesh274@gmail.com>
2026-07-13 23:36:21 +0100

    Time: 2118 ms (36.47%), Space: 13.3 MB (36.22%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        array=[]
+        inital=0
+
+        for j in range(len(nums)):
+            for i in range(j+1,len(nums)):
+                if nums[j]+nums[i]==target:
+                    return j,i
